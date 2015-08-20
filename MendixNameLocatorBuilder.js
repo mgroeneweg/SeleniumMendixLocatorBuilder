0305e7a368d7d7954d00151e1ff80297cc39c81a
Marcel Groeneweg <marcel.groeneweg@itvisors.com>
2015-08-20 17:49:24 +0200

    Initial release
    Initial release

---
+++ MendixNameLocatorBuilder.js
@@ -0,0 +1,73 @@
+/*jslint plusplus: true */
+/*global Selenium, LocatorBuilders, alert, browser, devel, console */
+
+LocatorBuilders.add('css:MendixName', function (e) {
+    'use strict';
+	var PFX_NAME = 'mx-name-',
+        PFX_NAME_INDEX = 'mx-name-index-',
+        className,
+        continueLoop,
+        currentElement,
+        findCssClassWithPrefix,
+        hasMxNameClass = false,
+        loopCount,
+        result;
+    
+    findCssClassWithPrefix = function (element, classPrefix) {
+        var className,
+            count = element.classList.length,
+            i;
+        for (i = 0; i < count; i++) {
+            className = element.classList.item(i);
+            if (className.indexOf(classPrefix) > -1) {
+                return '.' + className;
+            }
+        }
+        return null;
+    };
+    
+    // Does the element have a name class?
+	className = findCssClassWithPrefix(e, PFX_NAME);
+	if (className) {
+        // Found the name class directly on the element so use it.
+		result = className;
+        hasMxNameClass = true;
+        
+	} else {
+        // Element has no name class, take the node name as final part of the CSS
+        result = e.nodeName.toLowerCase();
+    }
+
+    continueLoop = true;
+    loopCount = 0;
+    currentElement = e;
+    while (continueLoop) {
+        loopCount++;
+        currentElement = currentElement.parentElement;
+        if (currentElement === null || currentElement === undefined || loopCount > 50) {
+            continueLoop = false;
+        } else {
+            // Only if name class has not already been found
+            if (!hasMxNameClass) {
+                className = findCssClassWithPrefix(currentElement, PFX_NAME);
+                if (className) {
+                    result = className + ' ' + result;
+                    hasMxNameClass = true;
+                }
+            }
+            // Look for classes that identify a template grid or list view row
+            className = findCssClassWithPrefix(currentElement, PFX_NAME_INDEX);
+            if (className) {
+                result = className + ' ' + result;
+            }
+        }
+    }
+    
+    if (hasMxNameClass) {
+        return 'css=' + result;
+    } else {
+        return null;
+
+    }
+//	return 'css=.mx-name-textBox2 input';
+});
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # SeleniumMendixLocatorBuilder
 Selenium locator builder for easy Mendix test recording
+
+Selenium can be used to test Mendix 5 applications as described here:
+https://world.mendix.com/display/howto50/Testing+Mendix+applications+using+Selenium+IDE
+
+However, you have to manually correct the locators in the recorded steps because HTML tag IDs are generated dynamically and will be different with each run of the same page.
+
+This is tedious work, so let the tools do the dirty work please...
+
+That is where this Selenium plugin steps in: When added to the Selenium IDE, the test steps will have mx-name- CSS selectors rather than the Selenium default. It will also look for parent grids and list views and add selectors to get the correct row by it's index.
+
+## Installation
+Steps to install:
+
+- Download MendixNameLocatorBuilder.js from the repository and save it on your local disk
+- Open the Selenium options dialog
+- At the general tab, select the downloaded file as IDE extension and click OK
+- Restart Selenium
+- Open the Selenium options dialog again.
+- At the Locator Builders tab, drag css:MendixName to the top.
+- Restart Selenium (to be safe)
+- Happy recording! :-)
+
+## Use cases covered
+
+### Using https://ux.mendix.com/
+
+- Buttons
+- Form input elements, dates too (input date value, not by using the calendar popup)
+- Tabs
+- Data grid rows
+- Clicking a button in a template grid and listview
+
+
+### Using my own apps
+
+- Reference selectors
+- Enumeration dropdowns
+- Double click a datagrid row to open details. (Selenium records two clicks, delete one and change the other into doubleclick)
+- Items in nested template grids
